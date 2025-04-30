c410244c76ee0fd5b029a60889c2f6fd54262fb2
Libertus <libert.jobs@gmail.com>
2025-05-01 00:26:38 +0700

    Belajar Query Builder 2

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -4,9 +4,14 @@ namespace App\Http\Controllers;
 
 use Illuminate\Support\Facades\DB;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Session;
+use Illuminate\Support\Str;
 
 class BlogController extends Controller
 {
+    /**
+     * Menampilkan seluruh dari tabel
+    */
     public function index(Request $request)
     {
         $cari = $request->cari;
@@ -15,4 +20,38 @@ class BlogController extends Controller
             ->paginate(5);
         return view('blog.index', compact('blogs', 'cari'));
     }
+
+    /**
+     * Menampilkan form create
+    */
+    public function create()
+    { 
+        return view('blog.create');
+    }
+
+    /**
+     * Menyimpan data
+    */ 
+    public function store(Request $request)
+    {
+        $request->validate([
+            'title' => 'required',
+            'description' => 'required'
+        ],[
+            'title.required' => 'Judul tidak boleh kosong',
+            'description.required' => 'Deskripsi tidak boleh kosong',
+        ]);
+
+        $title = $request->title;
+
+        DB::table('blogs')->insert([
+            'title' => $title,
+            'slug' => Str::slug($title),
+            'description' => $request->description
+        ]);
+
+        Session::flash("success", "Berita berhasil disimpan!");
+        return redirect()->route('blog.index');
+    }
+
 }
--- routes/web.php
+++ routes/web.php
@@ -7,4 +7,4 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/blog', [BlogController::class, 'index'])->name('blog');
+Route::resource('blog', BlogController::class);
